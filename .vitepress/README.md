108130840a674e6f8e87901175dd798ea4810b71
luojinan <luojinan@zhuanzhuan.com>
2024-01-24 19:18:51 +0800

    优化获取图片宽高逻辑

--- .vitepress/README.md
+++ .vitepress/README.md
@@ -1,6 +1,6 @@
 
 - [ ] 画廊页面设计
-- [ ] 图片放大功能 [github](https://github.com/vuejs/vitepress/issues/854)
+- [x] 图片放大功能 [github](https://github.com/vuejs/vitepress/issues/854)
 
 ![](https://kingan-md-img.oss-cn-guangzhou.aliyuncs.com/blog/20240124152951388.png?x-oss-process=image/format,webp/resize,w_640)
 问题是不支持移动端双指缩放、拖拽移动，PC也仅仅是把原图宽高展示出来，不支持再放大缩小，从源码上看也仅仅是普通的全屏查看图片，并没有其他交互逻辑
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,7 +3,7 @@ import Layout from './Layout.vue'
 import { useRoute } from 'vitepress';
 import { nextTick, onMounted, watch } from 'vue';
 
-const getwh = (src:string):Promise<{ width: number, height: number }> => {
+const getwh = (src:string):Promise<{ width: number, height: number,ratio: number }> => {
   return new Promise((resolve, reject) => {
     const img = new Image();
     img.onload = function () {
@@ -11,10 +11,12 @@ const getwh = (src:string):Promise<{ width: number, height: number }> => {
       const height = this.height;
       resolve({
         width,
-        height
+        height,
+        ratio: height/width
       })
     };
-    img.src = src
+    // url 设置为最小，已节省流量，用来获取宽高比，实际使用时自行设置宽高
+    img.src = `${src}/resize,w_10`
   })
 }
 
@@ -22,22 +24,23 @@ export default {
   Layout,
   setup() {
     const route = useRoute();
-    const initZoom = async () => {
+    const initZoom = () => {
       const imgElements = document.querySelectorAll('main img')
       for (let i = 0; i < imgElements.length; i++) {
         const img = imgElements[i];
-        let src = img.getAttribute("src");
-        const { width, height } = await getwh(src!)
+        let originSrc = img.getAttribute("src");
         // 原图
-        if (src?.includes('/resize,')) {
-          src = src.split('/resize,')[0]
+        let newSrc = originSrc
+        if (originSrc?.includes('/resize,')) {
+          newSrc = originSrc.split('/resize,')[0]
         }
         img.onclick = async () => {
+          const { ratio } = await getwh(newSrc!)
           const pswp = new PhotoSwipe({
             dataSource: [{
-              src,
-              width,
-              height,
+              src: newSrc,
+              width: 750 ,
+              height: ratio * 750,
               index: 0,
             }]
           });
