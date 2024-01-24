7326ddb5398ba54576726da2fa1347b5f1718077
luojinan <luojinan@zhuanzhuan.com>
2024-01-24 15:47:17 +0800

    图片放大方案一mediumZoom

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,6 +1,35 @@
 import './style.css'
 import Layout from './Layout.vue'
+import { useRoute } from 'vitepress';
+import { nextTick, onMounted, watch } from 'vue';
+import mediumZoom from 'medium-zoom';
+
+const imgHDSrc =()=>{
+  const imgElements = document.querySelectorAll("main img");
+  for (let i = 0; i < imgElements.length; i++) {
+    const img = imgElements[i];
+    const src = img.getAttribute("src");
+    if(src?.includes('/resize,')) {
+      img.setAttribute("data-zoom-src", src.split('/resize,')[0]);
+    }
+  }
+}
 
 export default {
-  Layout
+  Layout,
+  setup() {
+    const route = useRoute();
+    const initZoom = () => {
+      imgHDSrc()
+      mediumZoom('main img', { background: 'rgb(3 7 18)' });
+    };
+    onMounted(() => {
+      initZoom();
+    });
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    );
+  },
+
 }
