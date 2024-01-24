fbf84d9c81719f19762d9411eacaa01f0e653d5c
luojinan <luojinan@zhuanzhuan.com>
2024-01-24 16:57:33 +0800

    替换图片预览使用PhotoSwipe

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,9 +2,17 @@ import { defineConfig } from 'vitepress'
 
 export default defineConfig({
   base: "/lifeblog/",
-  title: 'My life is x',
+  title: "An's Blog",
   description: 'The life blog for me',
   head: [
-    ['meta', { name: 'twitter:site', content: '@vuejs' }]
+    ['meta', { name: 'twitter:site', content: '@vuejs' }],
+    [
+      'link',
+      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/photoswipe/5.4.2/photoswipe.min.css' }
+    ],
+    [
+      'script',
+      { async: '', src: 'https://cdnjs.cloudflare.com/ajax/libs/photoswipe/5.4.2/umd/photoswipe.umd.min.js' }
+    ],
   ],
 })
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,26 +2,48 @@ import './style.css'
 import Layout from './Layout.vue'
 import { useRoute } from 'vitepress';
 import { nextTick, onMounted, watch } from 'vue';
-import mediumZoom from 'medium-zoom';
 
-const imgHDSrc =()=>{
-  const imgElements = document.querySelectorAll("main img");
-  for (let i = 0; i < imgElements.length; i++) {
-    const img = imgElements[i];
-    const src = img.getAttribute("src");
-    if(src?.includes('/resize,')) {
-      img.setAttribute("data-zoom-src", src.split('/resize,')[0]);
-    }
-  }
+const getwh = (src:string):Promise<{ width: number, height: number }> => {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = function () {
+      const width = this.width;
+      const height = this.height;
+      resolve({
+        width,
+        height
+      })
+    };
+    img.src = src
+  })
 }
 
 export default {
   Layout,
   setup() {
     const route = useRoute();
-    const initZoom = () => {
-      imgHDSrc()
-      mediumZoom('main img', { background: 'rgb(3 7 18)' });
+    const initZoom = async () => {
+      const imgElements = document.querySelectorAll('main img')
+      for (let i = 0; i < imgElements.length; i++) {
+        const img = imgElements[i];
+        let src = img.getAttribute("src");
+        const { width, height } = await getwh(src!)
+        // 原图
+        if (src?.includes('/resize,')) {
+          src = src.split('/resize,')[0]
+        }
+        img.onclick = async () => {
+          const pswp = new PhotoSwipe({
+            dataSource: [{
+              src,
+              width,
+              height,
+              index: 0,
+            }]
+          });
+          pswp.init(); // initializing PhotoSwipe core adds it to DOM
+        };
+      }
     };
     onMounted(() => {
       initZoom();
@@ -31,5 +53,4 @@ export default {
       () => nextTick(() => initZoom())
     );
   },
-
 }
