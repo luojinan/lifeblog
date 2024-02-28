91cc600325fe28ed40472d35aabd9710b524c3e2
luojinan <luojinan@zhuanzhuan.com>
2024-02-29 00:19:32 +0800

    feat: 测试相册功能

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -5,14 +5,47 @@ import { nextTick, onMounted, watch } from 'vue';
 import { getImgWh } from './utils/index.js';
 
 
+// 创建弹窗
+const createPopup = (url: string) => {
+  const popup = document.createElement('div');
+  popup.className = 'popup';
+
+  const iframe = document.createElement('iframe');
+  iframe.src = url;
+  iframe.referrerPolicy = 'no-referrer';
+  iframe.width = '600';              // iframe的宽度
+  iframe.height = '400';             // iframe的高度
+  iframe.frameBorder = '0';          // iframe的边框（这里设为0表示无边框）
+  popup.appendChild(iframe);
+
+  const closeButton = document.createElement('button');
+  closeButton.innerText = 'Close';
+  closeButton.addEventListener('click', () => {
+    document.body.removeChild(popup);
+  });
+  popup.appendChild(closeButton);
+
+  document.body.appendChild(popup);
+};
+
 export default {
   Layout,
   setup() {
     const route = useRoute();
     const initZoom = () => {
       const imgElements = document.querySelectorAll('main img')
-      for (let i = 0; i < imgElements.length; i++) {
-        const img = imgElements[i] as HTMLElement;;
+      imgElements.forEach((img) => {
+        let imgalt = img.getAttribute("alt");
+        if (imgalt === 'iframe') {
+          // 这里设置属性好像迟了一步，还是要在 markdown-it渲染html时就设置好，找找自定义配置的地方
+          img.setAttribute("referrerpolicy", "no-referrer")
+          img.onclick = () => {
+            console.log('iframe预览')
+            createPopup('http://photocq.photo.store.qq.com/psc?/V11mIOg62J98n0/MoK.EY8GiNN2Kt4U8x1aWBQ*nOMFunxhsDiupqG6*nwvMgAn4Cdt2YBWVwoBvgzN14y*g2.yzWbrpMYPVdM6Xw!!/b')
+          }
+          return
+        }
+
         let originSrc = img.getAttribute("src");
         // 原图
         let newSrc = originSrc
@@ -21,10 +54,11 @@ export default {
         }
         img.onclick = async () => {
           const { ratio } = await getImgWh(newSrc!)
-          const pswp = new PhotoSwipe({
+          // TODO: PhotoSwipe 有点慢 丢到oss
+          const pswp = new window.PhotoSwipe({
             dataSource: [{
               src: newSrc,
-              width: 750 ,
+              width: 750,
               height: ratio * 750,
               index: 0,
             }],
@@ -32,7 +66,7 @@ export default {
           });
           pswp.init(); // initializing PhotoSwipe core adds it to DOM
         };
-      }
+      })
     };
     onMounted(() => {
       initZoom();
---
+++ posts/test.md
@@ -0,0 +1,19 @@
+---
+title: test
+date: 1900-01-01
+---
+
+- mnull 是小图
+- b 是大图
+
+![iframe](http://photocq.photo.store.qq.com/psc?/V11mIOg62J98n0/MoK.EY8GiNN2Kt4U8x1aWBQ*nOMFunxhsDiupqG6*nwvMgAn4Cdt2YBWVwoBvgzN14y*g2.yzWbrpMYPVdM6Xw!!/b)
+
+![iframe](http://photocq.photo.store.qq.com/psc?/V11mIOg62J98n0/MoK.EY8GiNN2Kt4U8x1aWE*zFgzImkFO*ruArfGynALZ2clfpOVX.Ur7PJpSUmbCNRBL3Qx17s0mZk038di5AQ!!/b)
+
+![iframe](http://photocq.photo.store.qq.com/psc?/V11mIOg62J98n0/MoK.EY8GiNN2Kt4U8x1aWJW2E0KvHy.Gcn.1kH8xY9PGygj3ae0TL0qtsmortYi8rrUYSMg70HVBBfYhs4QtPQ!!/b)
+
+![iframe](http://photocq.photo.store.qq.com/psc?/V11mIOg62J98n0/p**PzQuuUKSOEL6toAfIz8pPG.F5Fw9I.FgHUOFXA6jH1w7SVGRhQiqaaSdRD2jPdIMz3jQ*wSnolx3C0jn0ag!!/b)
+
+![iframe](http://photocq.photo.store.qq.com/psc?/V11mIOg62J98n0/p**PzQuuUKSOEL6toAfIz4n3g2kHrkw5Khkc13Et5HoYvZnnLlrByo4ruiszXpl7TCpdc4V7VIGMQzkfSYSd3Q!!/b)
+
+![iframe](http://photocq.photo.store.qq.com/psc?/V11mIOg62J98n0/MoK.EY8GiNN2Kt4U8x1aWE.E58nT3cse3iVH5rdCr.fS0ckmmsVOxKQjOhwFOYi*lm7iBo**734dr0FLBZ8EOw!!/b)
